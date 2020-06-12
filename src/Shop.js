815fea924ddfa29989976779e6b658562bd7cd5a
hansa-lallu <hansalallu@gmail.com>
2020-06-12 11:49:50 +0100

    refactoring sulfras item

--- src/Shop.js
+++ src/Shop.js
@@ -1,18 +1,26 @@
+const SULFURAS_ID = 'Sulfuras, Hand of Ragnaros'
+const AGED_BRIE_ID = 'Aged Brie'
+const CONJURED_ID = 'Conjured'
+const BACKSTAGE_PASS_ID = 'Backstage passes to a TAFKAL80ETC concert'
+
 class Shop {
   constructor(items=[]){
     this.items = items;
   }
-  updateQuality() {
-    for (var i = 0; i < this.items.length; i++) {
+
+ updateQuality() {
+    for (let i = 0; i < this.items.length; i++) {
+      if (this.isSulfurasItem(this.items[i])) {
+        return this.items;
+      }
+      
       if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
         if (this.items[i].quality > 0) {
-          if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
             if (this.items[i].name.includes("Conjured")) {
               this.items[i].quality = this.items[i].quality - 2;
             } else {
               this.items[i].quality = this.items[i].quality - 1;
             }
-          }
         }
       } else {
         if (this.items[i].quality < 50) {
@@ -31,16 +39,12 @@ class Shop {
           }
         }
       }
-      if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
         this.items[i].sellIn = this.items[i].sellIn - 1;
-      }
       if (this.items[i].sellIn < 0) {
         if (this.items[i].name != 'Aged Brie') {
           if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
             if (this.items[i].quality > 0) {
-              if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
                 this.items[i].quality = this.items[i].quality - 1;
-              }
             }
           } else {
             this.items[i].quality = this.items[i].quality - this.items[i].quality;
@@ -52,8 +56,13 @@ class Shop {
         }
       }
     }
+    
 
     return this.items;
+  }
+
+  isSulfurasItem(item) {
+    return item.name === SULFURAS_ID;
   }
 }
 
